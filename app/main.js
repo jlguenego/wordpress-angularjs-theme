38bfc0e78836d8784da4a22e92c52867eba6b914
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-07-16 11:25:06 +0200

    add: gulp config ok

--- app/main.js
+++ app/main.js
@@ -4,10 +4,8 @@ import './jlg-layout/jlg-layout.js';
 import './jlg-route/jlg-route.js';
 
 angular.module('main', [
-		'jlg-layout',
-		'jlg-route',
-	])
-	.config((jlgWordpressProvider) => {
-		'ngInject';
-		jlgWordpressProvider.url('http://localhost/repos/wordpress-example/wp-json');
-	});
+	'jlg-layout',
+	'jlg-route',
+]);
+
+require('./config.js');
--- app/vendors.js
+++ app/vendors.js
@@ -2,5 +2,3 @@ import 'angular/angular-csp.css';
 import 'angular';
 
 import 'font-awesome/css/font-awesome.css';
-
-
